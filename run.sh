389f4c6ebe1bc964bec315789735dd3460dbc104
jakobottar <jakobottar@pm.me>
2023-01-26 17:17:50 -0700

    minor tweaks

--- run.sh
+++ run.sh
@@ -9,4 +9,5 @@ echo "launching mlflow tracking server..."
 mlflow server \
     --backend-store-uri postgresql+psycopg2://${DB_USER}:${DB_PASSWD}@${DB_HOST}/${DB_NAME} \
     --default-artifact-root ${ARTIFACT_ROOT} \
-    --host 0.0.0.0
+    --host 0.0.0.0 \
+    --port 5000
